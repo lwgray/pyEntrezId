75a8a2e845076a0d0a86b63cb8c0814ca284d603
Larry Gray <lwgray@gmail.com>
2015-10-17 12:54:31 -0400

    upgrade version, added required_installs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name = 'PyEntrezId',
     packages = ['PyEntrezId'],
-    version = '1.0',
+    version = '1.1',
     description = 'Converts Ensembl Transcript Gene Id to Entrez Gene Id',
     author = 'Larry Gray',
     author_email = 'lwgray@gmail.com',
@@ -10,4 +10,5 @@ setup(
     download_url = 'https://github.com/lwgray/pyEntrezId/tarball/1.0',
     keywords = ['Ensembl', 'Entrez', 'Gene'],
     classifiers = [],
+    install_requires=['xmltodict>=0.9.2', 'requests>=2.8.1']
 )
