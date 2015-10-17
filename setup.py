469e0628c97860dc5edd37e98d1f28002045b435
Larry Gray <lwgray@gmail.com>
2015-10-17 16:15:57 -0400

    Fix tarbal url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author = 'Larry Gray',
     author_email = 'lwgray@gmail.com',
     url = 'https://github.com/lwgray/pyEntrezId',
-    download_url = 'https://github.com/lwgray/pyEntrezId/tarball/1.0',
+    download_url = 'https://github.com/lwgray/pyEntrezId/tarball/1.2',
     keywords = ['Ensembl', 'Entrez', 'Gene', 'HGNC'],
     classifiers = [],
     install_requires=['xmltodict>=0.9.2', 'requests>=2.8.1']
