8ae3952fd1e7c7d5cff555d8d3c957a5dc11a167
Larry Gray <lwgray@gmail.com>
2015-10-19 13:19:36 -0400

    1.  Update Pypi  2. Update test.py with proper function name

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name = 'PyEntrezId',
     packages = ['PyEntrezId'],
-    version = '1.4',
+    version = '1.4.1',
     description = 'Converts Entrez Gene Id to UniProt, HGNC ID to Entrez Gene Id, Ensembl gene Transcript Id to Entrez Gene Id',
     author = 'Larry Gray',
     author_email = 'lwgray@gmail.com',
     url = 'https://github.com/lwgray/pyEntrezId',
-    download_url = 'https://github.com/lwgray/pyEntrezId/tarball/1.4',
-    keywords = ['Ensembl', 'Entrez', 'Gene', 'HGNC'],
+    download_url = 'https://github.com/lwgray/pyEntrezId/tarball/1.4.1',
+    keywords = ['Ensembl', 'Entrez', 'Gene', 'HGNC', 'UniProt'],
     classifiers = [],
     install_requires=['xmltodict>=0.9.2', 'requests>=2.8.1']
 )
