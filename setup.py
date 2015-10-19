ae0eb1196d35f8b97b58ea1ed1f50737835fba4c
Larry Gray <lwgray@gmail.com>
2015-10-19 14:12:40 -0400

    Update Pypi Package to version 1.4.3, minor changes: Change Formatting of Readme.txt to be more RestructuredText compliant

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'PyEntrezId',
     packages = ['PyEntrezId'],
-    version = '1.4.2',
+    version = '1.4.3',
     description = 'Converts UniProt, HGNC, and Ensembl Transcript Ids to Entrez Gene Id',
     author = 'Larry Gray',
     author_email = 'lwgray@gmail.com',
     url = 'https://github.com/lwgray/pyEntrezId',
-    download_url = 'https://github.com/lwgray/pyEntrezId/tarball/1.4.2',
+    download_url = 'https://github.com/lwgray/pyEntrezId/tarball/1.4.3',
     keywords = ['Ensembl', 'Entrez', 'Gene', 'HGNC', 'UniProt'],
     classifiers = [],
     install_requires=['xmltodict>=0.9.2', 'requests>=2.8.1']
