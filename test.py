39cf4fcbfe2a3f584d362f5017e37e628ce81ad6
Larry Gray <lwgray@gmail.com>
2015-10-19 13:17:29 -0400

    correct test.py with proper function name

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from pyEntrezId import Conversion
 
 geneid = 'ENST00000407559'
 transcriptid = Conversion()
-entrezid = transcriptid.convert(geneid)
+entrezid = transcriptid.convert_ensembl_to_entrez(geneid)
 
 print "Ensembl Gene Transcript Id:  {0}".format(geneid)
 print "Entrez Gene Id: {0}".format(entrezid)
