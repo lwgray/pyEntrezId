84180ab66e5bb55addbacffb9f8b25edb8ee99da
lwgray <lwgray@gmail.com>
2016-05-07 20:40:34 -0400

    Removed Failing Test that was Attempting to check type of imported class

--- tests/tests.py
+++ tests/tests.py
@@ -1,18 +1,9 @@
-from PyEntrezId.Conversion import Conversion
 import unittest
-
 
 class TddinConversion(unittest.TestCase):
     ''' Define tests '''
-    def _getTargetClass(self):
-        from PyEntrezId.Conversion import Conversion
-        return Conversion
-
     def test_email(self):
         ''' Test email address syntax '''
+        from PyEntrezId.Conversion import Conversion
         email = 'lwgray.com'
         self.assertRaises(ValueError, Conversion, email)
-
-    def test_conversion_object(self):
-        conversion_object = Conversion('lwgray@gmail.com')
-        assert isinstance(conversion_object, self._getTargetClass)
