130433ae4bbbf456bb5b257a47e703ce0a662c45
lwgray <lwgray@gmail.com>
2016-05-09 20:53:18 -0400

    Clean branch for only tests

--- new.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-import json
-def full_ensembl_report(ensembl):
-    id = Conversion('lwgray@gmail.com')
-    entrez = id.convert_ensembl_to_entrez(ensembl)
-    uniprot = id.convert_entrez_to_uniprot(entrez)
-    return json.dumps({'Ensembl':ensembl, 'Entrez':entrez, 'Uniprot': uniprot})
